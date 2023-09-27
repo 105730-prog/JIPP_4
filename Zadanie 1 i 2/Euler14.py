eca52082575b2c290f81161da0bc688609f85ae9
Arek Jaw <73388566+105730-prog@users.noreply.github.com>
2023-09-27 17:15:32 +0200

    Update Euler14.py

--- Zadanie 1 i 2/Euler14.py
+++ Zadanie 1 i 2/Euler14.py
@@ -1,30 +1,27 @@
 # The following iterative sequence is defined for the set of positive integers
 # Which starting number, under one million, produces the longest chain ?
 def collatz_sequence(n):
+    def collatz_step(x):
+        if x % 2 == 0:
+            return x // 2
+        else:
+            return 3 * x + 1
+
     sequence = [n]
     while n != 1:
-        if n % 2 == 0:
-            n = n // 2  # if n is even, divide it by 2
-        else:
-            n = 3 * n + 1 # if odd, multiply it by 3 and add 1
+        n = collatz_step(n)
         sequence.append(n)
     return sequence
 
+def find_longest_collatz_sequence(limit):
+    def sequence_length(n):
+        return len(collatz_sequence(n))
 
-def euler_14():
-    max_length = 0
-    max_starting_number = 0
+    starting_numbers = range(1, limit)
+    return max(starting_numbers, key=sequence_length)
 
-    for i in range(1, 1000000):
-        sequence_length = len(collatz_sequence(i))
-        if sequence_length > max_length:
-            max_length = sequence_length
-            max_starting_number = i
-
-    return max_starting_number
-
-
-print(euler_14())
+result = find_longest_collatz_sequence(1000000)
+print(result)
 
 
 
